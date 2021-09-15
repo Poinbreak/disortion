9f39c8bfb30ffd9fcd9ce631703769eb2239d7c8
Poinbreak <90777067+Poinbreak@users.noreply.github.com>
2021-09-15 19:26:40 +0530

    Add files via upload

---
+++ playermovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+public class playermovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+	public float runSpeed = 40f;
+	float horizontalMove = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+	}
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false,false);
+	}
+}
